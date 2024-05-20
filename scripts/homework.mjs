e7b0f16bc10ae7b266b634afbcf9c5b8174ae074
LiliyaLexx <liliya.fedyurina@gmail.com>
2024-05-20 16:03:59 -0700

    third part is started

--- scripts/homework.mjs
+++ scripts/homework.mjs
@@ -1,3 +1,5 @@
+ //part 1
+ 
  document.addEventListener('DOMCOntentLoaded', () =>{
     const registrationForm = document.getElementById('registration');
     const loginForm = document.getElementById('login');
@@ -19,9 +21,25 @@
             showError('Passwords do not match.');
             return;
         }
-         // If all checks pass, you can submit the form or handle registration logic
+  
+        if(username.length < 3){
+            showError("Username must be more then 3 characters long.", registrationForm.username);
+            return
+        }
 
-         registrationForm.onsubmit();
+        // Validate email format (HTML5 email input does this automatically)
+
+        if(!validateEmail(email)){
+            showError('Please enter a valid email address.', registrationForm.email);
+        }
+
+         // Validate terms checkbox
+         if(!terms){
+            showError('You must agree to the Terms of Use.', registrationForm.terms)
+         }
+         // If all checks pass, you can submit the form or handle registration logic
+         alert('Form submitted successfully');
+         registrationForm.submit();
     });
 
     loginForm.addEventListener('submit', (event) => {
@@ -38,6 +56,28 @@
     function showError(message){
         errorDisplay.style.display = 'block';
         errorDisplay.innerHTML = `<p>${message}</p>`;
+        element.focus();
+    }
+    function validateUsername(username){
+        if(username.length ===0){
+            showError('Username cannot be blank.', registrationForm.username);
+            return;
+        }
+        if(username.length < 4){
+            showError('Username must be at least 4 characters.', registrationForm.username);
+            return false;
+        }
+        const uniqueChars = new Set(username){
+            if(uniqueChars.size < 2){
+                showError('Usernaem must contain at least two unique character', registrationForm.username);
+                return false;
+            }
+            if(/[^a-zA-Z0-9]/.test(username)){
+                showError('Username cannot contain any special characters or space', registrationForm.username);
+            }
+
+        }
+
     }
 
  } )
